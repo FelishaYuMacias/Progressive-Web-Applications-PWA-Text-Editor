23ddfdccd2b72ae6eaef2ac9a7286fed80a66942
Felisha <felisha.j.yu@gmail.com>
2022-11-29 15:09:24 -0800

    updated webpack file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,15 +3,16 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
+      database: './src/js/database.js',
+      editor: './src/js/editor.js',
+      header: './src/js/header.js',
     },
     output: {
       filename: '[name].bundle.js',
@@ -21,7 +22,7 @@ module.exports = () => {
         // Webpack plugin that generates our html file and injects our bundles. 
         new HtmlWebpackPlugin({
           template: './index.html',
-          title: 'Jate'
+          title: 'JATE'
         }),
        
         // Injects our custom service worker
@@ -34,8 +35,8 @@ module.exports = () => {
         new WebpackPwaManifest({
           fingerprints: false,
           inject: true,
-          name: 'Jate',
-          short_name: 'Jate',
+          name: 'Just Another Text Editor',
+          short_name: 'JATE',
           description: 'Just Another Text Editor',
           background_color: '#225ca3',
           theme_color: '#225ca3',
@@ -53,6 +54,7 @@ module.exports = () => {
 
     module: {
       rules: [
+        //CSS Loaders
         {
           test: /\.css$/i,
           use: ['style-loader', 'css-loader'],
