8e868a15ef1b907efb507a160b57abbe445a8db0
Felisha <felisha.j.yu@gmail.com>
2022-11-29 15:25:52 -0800

    updated install.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,7 +15,7 @@ const initdb = async () =>
 // Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => 
 {
-  console.log('Post to the database');
+  console.log('PUT to the database');
 
   // Create a connection to the database database and version we want to use.
   const jateDb = await openDB('jate', 1);
@@ -26,13 +26,12 @@ export const putDb = async (content) =>
   // Open up the desired object store.
   const store = tx.objectStore('jate');
 
-  // Use the .add() method on the store and pass in the content.
-  const request = store.add({ content:content });
+  // Use the .put() method on the store and pass in the content.
+  const request = store.put({ id: 1, value: content });
 
   // Get confirmation of the request.
   const result = await request;
   console.log('🚀 - data saved to the database', result);
-  console.error('putDb not implemented');
 };
 
 // Add logic for a method that gets all the content from the database
@@ -55,8 +54,7 @@ export const getDb = async () =>
   // Get confirmation of the request.
   const result = await request;
   console.log('result.value', result);
-  console.error('getDb not implemented');
-  return result;
+  return result?.value;
 };
 
 initdb();
