67abe4908d7cc2b20f482102e4a8e21756910642
Felisha <felisha.j.yu@gmail.com>
2022-11-29 10:01:33 -0800

    added asset caching to src-sw

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,17 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+   // Here we define the callback function that will filter the requests we want to cache (in this case, JS and CSS files)
+   ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+   new CacheFirst({
+     // Name of the cache storage.
+     cacheName: 'asset-cache',
+     plugins: [
+       // This plugin will cache responses with these headers to a maximum-age of 30 days
+       new CacheableResponsePlugin({
+         statuses: [0, 200],
+       }),
+     ],
+   })
+);
