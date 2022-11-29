d126a2b9554d70cc7d86c390e5ded3e2634aa113
Felisha <felisha.j.yu@gmail.com>
2022-11-29 15:12:48 -0800

    updated src-sw

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,5 +1,5 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
@@ -30,14 +30,16 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 registerRoute(
    // Here we define the callback function that will filter the requests we want to cache (in this case, JS and CSS files)
    ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
-   new CacheFirst({
+   new StaleWhileRevalidate({
      // Name of the cache storage.
      cacheName: 'asset-cache',
      plugins: [
-       // This plugin will cache responses with these headers to a maximum-age of 30 days
-       new CacheableResponsePlugin({
-         statuses: [0, 200],
-       }),
-     ],
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxAgeSeconds: 30 * 24 * 60 * 60,
+      }),
+    ],
    })
 );
